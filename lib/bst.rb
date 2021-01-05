a4d0b42ca1875649db0df19f5eb093c1fc5f7b00
Harold Parra <gc.harold.parra@gmail.com>
2021-01-05 19:31:57 +0000

    added insert, delete and rebalance methods for Tree class

--- lib/bst.rb
+++ lib/bst.rb
@@ -8,8 +8,10 @@ class Tree
   def build_tree(array)
     if array.length == 1
       Node.new(array[0], nil, nil)
+
     elsif array.length == 2
       Node.new(array[1], Node.new(array[0], nil, nil), nil)
+      
     else
       left_tree = array.slice!(0...array.length/2)
       temp_root = array.slice!(0)
@@ -22,23 +24,45 @@ class Tree
   def inorder_array(node = @root)
     if node.left == nil && node.right == nil then
       [node.value]
+
     elsif node.left == nil
       [node.value] + inorder_array(node.right) 
+
     elsif node.right == nil
       inorder_array(node.left) + [node.value]
+
     else
       inorder_array(node.left) + [node.value] + inorder_array(node.right)
+
     end
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
-    if node.right
-      pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) 
-    end
+      pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+
       puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
-    if node.left
-      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) 
-    end
+
+      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value, node = @root)
+    @root = build_tree(self.inorder_array().push(value).sort.uniq)
+  end
+
+  #ALTERNATIVE INSERT
+    #if value >= node.value then
+      #node.right == nil ? node.right = Node.new(value, nil, nil) : insert(value, node.right)
+    #else
+      #node.left == nil ? node.left = Node.new(value, nil, nil) : insert(value, node.left)
+    #end
+
+  def delete(value, node = @root)
+    self.inorder_array().include?(value) ? @root = build_tree(self.inorder_array().delete(value))
+    : (puts "No such value in tree")
+  end
+
+  def rebalance(node = @root)
+    @root = build_tree(self.inorder_array().uniq)
   end
 end
 
@@ -51,8 +75,3 @@ class Node
     @left = left
   end
 end
-
-
-p [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324].sort.uniq
-bst = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-p bst.inorder_array
